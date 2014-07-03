93e1ec6c15d5b183e5b0f284894175d2241259b8
Roach_ <roachxxxx@gmail.com>
2014-07-03 22:12:08 +0300

    Fixed Bug
    - Fixed Target Type Selection

--- Pantheon - The Artisan of War.lua
+++ Pantheon - The Artisan of War.lua
@@ -1,4 +1,4 @@
-local version = "4.11"
+local version = "4.12"
 --[[
 
 
@@ -638,7 +638,7 @@ function GetCustomTarget()
     	return _G.MMA_Target
    	elseif _G.AutoCarry and _G.AutoCarry.Crosshair and _G.AutoCarry.Attack_Crosshair then
    		return _G.AutoCarry.Attack_Crosshair.target
-   	elseif TargetSelector.target and not TargetSelector.target.dead and TargetSelector.target.type  == "obj_AI_Hero" then
+   	elseif TargetSelector.target and not TargetSelector.target.dead and TargetSelector.target.type  == myHero.type then
     	return TargetSelector.target
     else
     	return nil
